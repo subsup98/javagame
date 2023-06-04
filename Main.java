5e7ecdeb92b65c5415ab20a5b4ce0e8fda3b4087
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2023-06-04 20:57:24 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,18 @@
 
 
-package pokemongame_;
-
-
 public class Main 
 {
 
 	public static void main(String[] args) 
 	{   
-
+		//Bgm intro = new Bgm("src/bgm/intro.mp3");
+		//intro.play();
 		Message.title();
 		Pokemon[] mypok = Message.start();
-		Player player = new Player("playername");
 		//intro.close();
-		Story.town_main(View.name_town[View.mytown], mypok, player);
+		Story.town_main(View.name_town[View.mytown], mypok);
 		
 	}
 
+
 }
-
-
-
-
-
